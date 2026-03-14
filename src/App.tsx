2d925d80b8d71fc3718fa07b0dc5f9e9d025b67c
incepthink <incepthink@gmail.com>
2026-03-14 12:14:17 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/examples/ecommerce">
         <StoreProvider>
           <Navbar />
           <Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/examples/ecommerce/',
   plugins: [react()],
   resolve: {
     alias: {
